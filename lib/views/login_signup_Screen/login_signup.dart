7e4c018eff8c5fca836a9757656609df1609d402
satyam379prasad@gmail.com <satyam@navigolabs.com>
2025-08-25 23:09:19 +0530

    extract the signup and created a new folder widget on loginsignup folder

--- lib/views/login_signup_Screen/login_signup.dart
+++ lib/views/login_signup_Screen/login_signup.dart
@@ -123,47 +123,7 @@ class _LoginScreenState extends State<LoginSignup> {
                           },
                           children: [
                             LoginForm(isChecked: isChecked),
-                            SingleChildScrollView(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(height: ScreenUtil.height(3)),
-                                  TextFieldHeading(text: 'Full Name'),
-                                  CustomTextField(onChanged: (value) {}),
-                                  SizedBox(height: ScreenUtil.height(2)),
-                                  TextFieldHeading(text: 'Email'),
-                                  CustomTextField(onChanged: (value) {}),
-                                  SizedBox(height: ScreenUtil.height(2)),
-                                  TextFieldHeading(text: 'Phone Number'),
-                                  CustomTextField(onChanged: (value) {}),
-                                  SizedBox(height: ScreenUtil.height(2)),
-                                  TextFieldHeading(text: 'Password'),
-                                  CustomTextField(
-                                    onChanged: (value) {},
-                                    icon: Icon(Icons.visibility),
-                                    obsecure: true,
-                                  ),
-                                  SizedBox(height: ScreenUtil.height(2)),
-                                  TextFieldHeading(text: 'Confirm Password'),
-                                  CustomTextField(
-                                    onChanged: (value) {},
-                                    icon: Icon(Icons.visibility),
-                                    obsecure: true,
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.only(
-                                      top: ScreenUtil.height(4),
-                                    ),
-                                    child: Center(
-                                      child: ElevatedButton(
-                                        onPressed: () {},
-                                        child: Text('Sign Up'),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                            SignupForm(),
                           ],
                         );
                       },
@@ -171,6 +131,50 @@ class _LoginScreenState extends State<LoginSignup> {
                   ),
                 ],
               ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SignupForm extends StatelessWidget {
+  const SignupForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: ScreenUtil.height(3)),
+          TextFieldHeading(text: 'Full Name'),
+          CustomTextField(onChanged: (value) {}),
+          SizedBox(height: ScreenUtil.height(2)),
+          TextFieldHeading(text: 'Email'),
+          CustomTextField(onChanged: (value) {}),
+          SizedBox(height: ScreenUtil.height(2)),
+          TextFieldHeading(text: 'Phone Number'),
+          CustomTextField(onChanged: (value) {}),
+          SizedBox(height: ScreenUtil.height(2)),
+          TextFieldHeading(text: 'Password'),
+          CustomTextField(
+            onChanged: (value) {},
+            icon: Icon(Icons.visibility),
+            obsecure: true,
+          ),
+          SizedBox(height: ScreenUtil.height(2)),
+          TextFieldHeading(text: 'Confirm Password'),
+          CustomTextField(
+            onChanged: (value) {},
+            icon: Icon(Icons.visibility),
+            obsecure: true,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: ScreenUtil.height(4)),
+            child: Center(
+              child: ElevatedButton(onPressed: () {}, child: Text('Sign Up')),
             ),
           ),
         ],
